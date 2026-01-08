0a00b64a03b4519742e618a5efc3ebdc005d5df8
Mami <L2507934395as@gmail.com>
2026-01-08 14:22:04 +0900

    第20回課題

--- src/main/java/student/manatement/StudentManegement/controller/StudentController.java
+++ src/main/java/student/manatement/StudentManegement/controller/StudentController.java
@@ -5,7 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
